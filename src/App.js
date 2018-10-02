0a8cd5ece532352849b5032a1acadd48e7016003
Monica Howard <imonicahoward@Monicas-iMac.attlocal.net>
2018-10-01 20:29:04 -0500

    Deleted default css & Added styled-components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+
+const CustomElement = styled.div`
+  color: green;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Crypto Dashboard</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <CustomElement>
+        Hello
+      </CustomElement>
     );
   }
 }
