fabf14f0788650bb1824638052603210035d793d
Monica Howard <imonicahoward@Monicas-iMac.attlocal.net>
2018-10-01 21:01:53 -0500

    Add App Bar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import './App.css';
 
-const CustomElement = styled.div`
+const Logo = styled.div`
+  font-size: 1.5em;
+`
 
+const ControlButton = styled.div`
+`
+
+const AppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 250px auto 200px 200px;
+  padding: 40px;
 `
 
 class App extends Component {
   render() {
     return (
-      <CustomElement>
-        Hello
-      </CustomElement>
+      <Bar>
+        <Logo>
+          Crypto Dashboard
+        </Logo>
+        <div>
+        </div>
+        <ControlButton>
+          Dashboard
+        </ControlButton>
+        <ControlButton>
+          Settings
+        </ControlButton>
+      </Bar>
     );
   }
 }
