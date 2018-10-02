a9f2c721907b6ed934931ca6b5d9f7e606744b85
Monica Howard <imonicahoward@Monicas-iMac.attlocal.net>
2018-10-01 23:01:16 -0500

    Add onclick function to button to dynamically toggle between active states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import './App.css';
 
 const Logo = styled.div`
@@ -7,30 +7,50 @@ const Logo = styled.div`
 `
 
 const ControlButton = styled.div`
+  cursor: pointer;
+  ${props => props.active && css`
+    text-shadow: 0px 0px 60px #F4E409`}
 `
-
 const AppLayout = styled.div`
-  display: grid;
-  grid-template-columns: 250px auto 200px 200px;
   padding: 40px;
 `
 
+const Bar = styled.div`
+  display: grid;
+  grid-template-columns: 250px auto 200px 200px;
+  margin-bottom: 40px;
+`
+
+const Content = styled.div`
+`
+
 class App extends Component {
+  state = {
+    page: 'dashboard'
+  }
+  showingDashboard = () =>  this.state.page  === 'dashboard';
+  showingSettings = () =>  this.state.page  === 'settings';
+
   render() {
     return (
-      <Bar>
-        <Logo>
-          Crypto Dashboard
-        </Logo>
-        <div>
-        </div>
-        <ControlButton>
-          Dashboard
-        </ControlButton>
-        <ControlButton>
-          Settings
-        </ControlButton>
-      </Bar>
+      <AppLayout>
+        <Bar>
+          <Logo>
+            Crypto Dashboard
+          </Logo>
+          <div>
+          </div>
+          <ControlButton onClick={()=>{this.setState({page: 'dashboard'})}} active = {this.showingDashboard()} >
+            Dashboard
+          </ControlButton>
+          <ControlButton onClick={()=>{this.setState({page: 'settings'})}} active = {this.showingSettings()}>
+            Settings
+          </ControlButton>
+        </Bar>
+        <Content>
+          Hello, I'm { this.state.page }
+        </Content>
+      </AppLayout>
     );
   }
 }
