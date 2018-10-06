585905c82b2afe531b069de7e5c2363b6177929b
Monica Howard <imonicahoward@Monicas-iMac.attlocal.net>
2018-10-05 22:37:38 -0500

    Updated styling to add hover effects on CoinTile | Added DeleteIcon

--- src/CoinList.js
+++ src/CoinList.js
@@ -44,6 +44,14 @@ const CoinSymbol = styled.div`
     justify-self: right;
 `
 
+const DeleteIcon = styled.div`
+    justify-self: right;
+    ${CoinTile}:hover & {
+        display: block;
+        color: red;
+    }
+`
+
 export default function(favorites=false) {
     let coinKeys = favorites ? this.state.favorites : Object.keys(this.state.coinList).slice(0, 100);
     return(
@@ -53,7 +61,7 @@ export default function(favorites=false) {
                 favorites ? () => {this.removeCoinFromFavorites(coinKey)} : () => {this.addCoinToFavorites(coinKey)}}>
                 <CoinHeaderGrid>
                     <div>{this.state.coinList[coinKey].CoinName}</div>
-                    <CoinSymbol>{this.state.coinList[coinKey].Symbol}</CoinSymbol>
+                    {favorites ? <DeleteIcon> X </DeleteIcon> : <CoinSymbol>{this.state.coinList[coinKey].Symbol}</CoinSymbol>}
                 </CoinHeaderGrid>
                     {<img style={{height: '50px'}} src={`http://cryptocompare.com/${this.state.coinList[coinKey].ImageUrl}`} />}
               
